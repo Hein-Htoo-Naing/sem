b3777550d1c77fe8fd4e02980d5fad4e072b2525
kaungmyatlwin18 <40841060@live.napier.ac.uk>
2026-09-17 15:50:23 +0630

    Configure Dockerfile with fat JAR, connect to mongo-dbserver with retry logic

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -7,13 +7,61 @@ import org.bson.Document;
 
 public class App
 {
+    private MongoClient mongoClient = null;
+
+    /**
+     * Connect to the MongoDB database with retry logic.
+     */
+    public void connect()
+    {
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Pause for 5 seconds to wait for MongoDB to initialize
+                Thread.sleep(5000);
+                // Connect to MongoDB container on the Docker bridge network
+                mongoClient = new MongoClient("mongo-dbserver", 27017);
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+            catch (Exception e)
+            {
+                System.out.println("Failed to connect to database attempt " + (i + 1));
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Disconnect from the MongoDB database.
+     */
+    public void disconnect()
+    {
+        if (mongoClient != null)
+        {
+            mongoClient.close();
+            System.out.println("Closed connection to database.");
+        }
+    }
+
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system using try-with-resources
-        try (MongoClient mongoClient = new MongoClient("localhost", 27000))
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        if (a.mongoClient != null)
         {
             // Get database
-            MongoDatabase database = mongoClient.getDatabase("mydb");
+            MongoDatabase database = a.mongoClient.getDatabase("mydb");
 
             // Get collection
             MongoCollection<Document> collection = database.getCollection("test");
@@ -29,9 +77,13 @@ public class App
 
             // Check document in collection
             Document myDoc = collection.find().first();
-            if (myDoc != null) {
+            if (myDoc != null)
+            {
                 System.out.println(myDoc.toJson());
             }
+
+            // Disconnect from database
+            a.disconnect();
         }
     }
 }
